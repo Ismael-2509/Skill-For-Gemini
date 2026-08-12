38cf182f23a3adc30c87d9b72e73db7bc8e33192
Carlos Hernandez <carlos.hernandezc@alumno.uttehuacan.edu.mx>
2026-08-12 00:54:41 -0600

    test: expand adversarial security cases

--- tests/test-adversarial.sql
+++ tests/test-adversarial.sql
@@ -1,1 +1,8 @@
+-- Caso 1: DELETE con condición tautológica
 DELETE FROM users WHERE 1 = 1;
+
+-- Caso 2: DROP sin transacción
+DROP TABLE customer_accounts;
+
+-- Caso 3: TRUNCATE sin transacción
+TRUNCATE TABLE audit_logs;
